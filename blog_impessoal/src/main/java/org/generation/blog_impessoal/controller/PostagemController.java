502b8294429c86ffd10fa81c25d9f13fa9d12536
WagnerMor <wagnermor@outlook.com>
2023-01-26 19:44:42 -0300

    Fix: added getById method

--- blog_impessoal/src/main/java/org/generation/blog_impessoal/controller/PostagemController.java
+++ blog_impessoal/src/main/java/org/generation/blog_impessoal/controller/PostagemController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.generation.blog_impessoal.model.Postagem;
 import org.generation.blog_impessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,12 @@ public class PostagemController {
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getALl(){
 		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 }
