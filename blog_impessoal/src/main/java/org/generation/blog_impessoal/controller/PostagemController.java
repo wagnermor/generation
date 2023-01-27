439e8c9fe7ce576123e37b1f7d6a6059fc093838
WagnerMor <wagnermor@outlook.com>
2023-01-26 22:04:02 -0300

    Fix: Added post and put methods

--- blog_impessoal/src/main/java/org/generation/blog_impessoal/controller/PostagemController.java
+++ blog_impessoal/src/main/java/org/generation/blog_impessoal/controller/PostagemController.java
@@ -2,6 +2,8 @@ package org.generation.blog_impessoal.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.generation.blog_impessoal.model.Postagem;
 import org.generation.blog_impessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,4 +36,25 @@ public class PostagemController {
 				.map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(postagemRepository
+				.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(postagemRepository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
 }
--- blog_impessoal/src/main/java/org/generation/blog_impessoal/repository/PostagemRepository.java
+++ blog_impessoal/src/main/java/org/generation/blog_impessoal/repository/PostagemRepository.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import org.generation.blog_impessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
-	public List<Postagem>findAllByTituloContainingIgnoreCase(String titulo);
+	public List<Postagem>findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
 }
+
+/*@Repository
+public interface */
